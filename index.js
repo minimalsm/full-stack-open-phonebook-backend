6a26c53375b41a29152e6a4bb1ae8d040b468d1d
Joshua <30259508@cityofglacol.ac.uk>
2020-06-09 20:32:17 +0100

    Ready for Heroku

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token("post-body", function(req) {
   return JSON.stringify(req.body);
@@ -116,7 +118,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
