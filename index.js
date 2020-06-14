2c783089641cbc52b5cf48d3d5c5cbf647aceb91
Joshua <30259508@cityofglacol.ac.uk>
2020-06-14 12:55:18 +0100

    Completed 3.17

--- index.js
+++ index.js
@@ -30,31 +30,12 @@ app.use(
   })
 )
 
-
-// let persons = [
-//   {
-//     "name": "Ada Lovelace",
-//     "number": "39-44-5323523",
-//     "id": 2
-//   },
-//   {
-//     "name": "Dan Abramov",
-//     "number": "12-43-234345",
-//     "id": 3
-//   },
-//   {
-//     "name": "Mary Poppendieck",
-//     "number": "39-23-6423122",
-//     "id": 4
-//   }
-// ]
-
-app.get('/info', (request, response) => {
-  response.send(`
-  <p>Phonebook has info for ${persons.length} people.</p>
-  <p>${Date(Date.now())}<p>
-  `)
-})
+// app.get('/info', (request, response) => {
+//   response.send(`
+//   <p>Phonebook has info for ${persons.length} people.</p>
+//   <p>${Date(Date.now())}<p>
+//   `)
+// })
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -62,9 +43,29 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
- Person.findById(request.params.id).then(person => {
-    response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(204).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+  
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
   })
 })
 
@@ -76,29 +77,19 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
 
-  // if (!body.name || !body.number) {
-  //   return response.status(400).json({
-  //     error: 'content missing'
-  //   })
-  // }
-
-  // if (persons.some(p => p.name === body.name)) {
-  //   return response.status(400).json({
-  //     error: 'name already exists'
-  //   })
-  // }
-
-  const person = new Person({
+  const person = {
     name: body.name,
     number: body.number,
-  })
+  }
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
